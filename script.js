32b4b421ad366f3209829fd99108362ed37c7b20
Pankaj Kumar <pankajkumar123014@gmail.com>
2023-04-26 18:06:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-function createTable() {
-    //Write your code here
-	let table = document.getElementById("myTable");
-	let rn =prompt(" input number of rows", "rn");
-	let cn=prompt(" input number of columns", "cn");
+
 	
-	function createTable()
+function createTable()
 {
-  let rn = window.prompt("Input number of rows", 1);
-let cn = window.prompt("Input number of columns",1);
+	let table = document.getElementById("myTable");
+	let rn = window.prompt("Input number of rows", 1);
+	let cn = window.prompt("Input number of columns",1);
   
  for(var r=0;r<parseInt(rn,10);r++)
   {
-   var x=document.getElementById('myTable').insertRow(r);
-   for(var c=0;c<parseInt(cn,10);c++)  
-    {
-     var y=  x.insertCell(c);
-     y.innerHTML="Row-"+r+" Column-"+c; 
-    }
-   }
+   var x=table.insertRow(r);
+	   for(var c=0;c<parseInt(cn,10);c++)  
+	    {
+	     var y=  x.insertCell(c);
+	     y.innerHTML="Row-"+r+" Column-"+c; 
+		}
+	}
 }
+	
